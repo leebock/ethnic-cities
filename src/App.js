41647e6e330d38129c3230b9fca32d16f8d443ae
leebock <lbock@esri.com>
2021-06-09 15:40:22 -0400

    addressed responsive column vs row

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
             {id: 3, name: "Naptown", state: "IN", lat: 39.77, lon: -86.16},
             {id: 4, name: "Stumptown", state: "OR", lat: 45.5, lon: -122.7},
             {id: 5, name: "Tinseltown", state: "CA", lat: 34.09, lon: -118.33},
-            {id: 6, name: "Twangtown", state: "TN", lat: 36.1, lon: -86.78},
+            {id: 6, name: "Twangtown", state: "TN", lat: 36.1, lon: -86.78}
         ]
     );
     
@@ -26,26 +26,26 @@ function App() {
     const cancelSelect = () => {
         setSelectedId(-1);
     }
-    
+
     return (
         <div className="container vh-100 d-flex flex-column">
             <header className="row mt-5 mb-3">
                 <h1>My First React App (with Bootstrap!)</h1>
             </header>
-            <div className="row d-flex overflow-hidden">
+            <div className="row d-flex flex-column flex-md-row overflow-hidden">
                 <MyMap className="col h-100" 
                     cities={cities} 
                     selectedId={selectedId}
                     onSelect={selectCity} 
                     onCancelSelect={cancelSelect}/>
-                <List className="col list-group h-100 overflow-auto"
+                <List className="col h-100 list-group overflow-auto"
                     cities={cities}
                     selectedId={selectedId}
                     onSelect={selectCity} 
                     onCancelSelect={cancelSelect}/>
             </div>
             <footer className="container mb-5">This is the footer.</footer>
-        </div>
+            </div>
     );
 
 }
