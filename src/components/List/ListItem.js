e893fbb27580eafeb1d7c3c127c5fc12451ac4f8
leebock <lbock@esri.com>
2021-06-09 18:47:03 -0400

    tweaks

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -7,9 +7,9 @@ export const ListItem = ({city, selected, onSelect}) => {
         <button key={city.id} className={className} onClick={handleItemClick}>
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-1">{city.name}</h5>
-            <small>Population: <strong>{(city.population).toLocaleString()}</strong></small>
+            <small className="mx-1">Population: <strong>{(city.population).toLocaleString()}</strong></small>
           </div>
-          <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <p className="mb-1">Population: <strong>{(city.population).toLocaleString()}</strong></p>
           <small>Donec id elit non mi porta.</small>
         </button>                        
     );
